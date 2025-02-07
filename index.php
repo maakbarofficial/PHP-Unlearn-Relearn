aa2a421d85ebd0621c90a77cb0f7b1972277ca29
maakbarofficial <maakbarofficial@gmail.com>
2025-02-07 19:00:39 +0500

    Adding PHP in HTML Code

--- index.php
+++ index.php
@@ -4,15 +4,36 @@ echo "Hello PHP.....!"
 
 
 
-// This is single comment
+    // This is single comment
 
-/*
+    /*
 
-This is multi line comment
+    This is multi line comment
 
-*/
+    */
 
 
-# This is also single line comment
+    # This is also single line comment
 
-?>
+    ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+
+    <h1><?php echo "This is H1" ?></h1>
+
+
+
+
+</body>
+
+</html>
