2b99cd3d01474ff065745a6ff61484a139ff669c
maakbarofficial <maakbarofficial@gmail.com>
2025-02-10 19:32:56 +0500

    Indexed Array

--- index.php
+++ index.php
@@ -1,58 +1,25 @@
 <?php
 
-$str = "Hello <br>";
+// Indexed Array
+$fruits = array("Apple", "Orange", "Banana", "Mango");
 
-for ($i = 0; $i < 10; $i++) {
-    echo "$i " . $str;
+array_push($fruits, "coconut", "pineapple");
+array_pop($fruits);
+array_shift($fruits);
+
+
+$reverseFruits = array_reverse($fruits);
+
+echo "<br>";
+
+echo count($reverseFruits);
+
+echo "<br>";
+
+foreach ($reverseFruits as $fruit) {
+    echo "$fruit <br>";
 }
 
-echo "<br><br>";
+echo "<br>";
 
-$n = 1;
-
-while ($n <= 10) {
-    echo "$n <br>";
-    $n++;
-}
-
-echo "Out of loop";
-
-?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loop</title>
-</head>
-
-<body>
-
-    <h1>One way of loop</h1>
-
-    <!-- $i = 1;
-
-    echo "<h1>While loop</h1>";
-
-    while ($i <= 10) {
-     echo "<p>This is $i</p>";
-        $i++;
-    } -->
-
-    <?php
-    $i = 1;
-    while ($i <= 10) {
-        $i++;
-    ?>
-        <h1>Loop in a different way</h1>
-        <p><?php echo "This is loop $i" ?></p>
-
-    <?php } ?>
-
-</body>
-
-</html>
+print_r($fruits);
