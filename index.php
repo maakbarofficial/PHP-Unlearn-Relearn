89b18116cc79eaf1c1aba86342eec7c6b50e3504
maakbarofficial <maakbarofficial@gmail.com>
2025-02-10 20:10:03 +0500

    Multidimensional Arrays

--- index.php
+++ index.php
@@ -1,26 +1,39 @@
 <?php
 
-// Associative Arrays
-// Associative arrays are arrays that use named keys that you assign to them
+// Multidimensional Arrays
+// A multidimensional array is an array containing one or more arrays.
+// PHP supports multidimensional arrays that are two, three, four, five, or more levels deep
 
-$employees = array(
-    "Akbar" => "Software Enginer",
-    "Babar" => "Web Developer",
-    "Ali" => "JavaScript Developer"
+$students = array(
+    array(
+        "Name" => "Ali Akbar",
+        "Role" => "Software Enginer",
+    ),
+    array(
+        "Name" => "Babar",
+        "Role" => "Senior Software Enginer",
+    ),
+    array(
+        "Name" => "Yasir",
+        "Role" => "Junior Software Enginer",
+    )
 );
-print_r(array_values($employees));
+// print_r($students);
 echo "<br><br>";
-print_r(array_keys($employees));
-echo "<br><br>";
-print_r(array_flip($employees));
-echo "<br><br>";
-print_r(array_reverse($employees));
-echo "<br><br>";
+// print_r(array_keys($employees));
+// echo "<br><br>";
+// print_r(array_flip($employees));
+// echo "<br><br>";
+// print_r(array_reverse($employees));
+// echo "<br><br>";
 
-?>
+foreach ($students as $key => $value) {
+    echo "<prev>";
+    // print_r($value);
+    echo "<br><br>";
 
-<?php
-foreach ($employees as $key => $value) {
-?>
-    <p><b><?php echo $key ?>: </b><?php echo $value ?></p>
-<?php } ?>
+    foreach ($value as $k => $v) {
+        echo "<br><br>";
+        echo $v . "<br>";
+    }
+}
