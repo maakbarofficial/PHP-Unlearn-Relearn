89f4d9a4e9468a54722a38280ccf8ac1b861e80d
maakbarofficial <maakbarofficial@gmail.com>
2025-02-10 19:33:37 +0500

    Indexed Array Detailed

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 // Indexed Array
+// In indexed arrays each item has an index number.
+// By default, the first item has index 0, the second item has item 1, etc.
 $fruits = array("Apple", "Orange", "Banana", "Mango");
 
 array_push($fruits, "coconut", "pineapple");
