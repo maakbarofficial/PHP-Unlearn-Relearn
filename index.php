62da64ee1493dad557b5ad9f47d510098dff86c6
maakbarofficial <maakbarofficial@gmail.com>
2025-02-10 19:59:13 +0500

    Associative Arrays

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?php
 
-// Indexed Array
-// In indexed arrays each item has an index number.
-// By default, the first item has index 0, the second item has item 1, etc.
-$fruits = array("Apple", "Orange", "Banana", "Mango");
+// Associative Arrays
+// Associative arrays are arrays that use named keys that you assign to them
 
-array_push($fruits, "coconut", "pineapple");
-array_pop($fruits);
-array_shift($fruits);
+$employees = array(
+    "Akbar" => "Software Enginer",
+    "Babar" => "Web Developer",
+    "Ali" => "JavaScript Developer"
+);
+print_r(array_values($employees));
+echo "<br><br>";
+print_r(array_keys($employees));
+echo "<br><br>";
+print_r(array_flip($employees));
+echo "<br><br>";
+print_r(array_reverse($employees));
+echo "<br><br>";
 
+?>
 
-$reverseFruits = array_reverse($fruits);
-
-echo "<br>";
-
-echo count($reverseFruits);
-
-echo "<br>";
-
-foreach ($reverseFruits as $fruit) {
-    echo "$fruit <br>";
-}
-
-echo "<br>";
-
-print_r($fruits);
+<?php
+foreach ($employees as $key => $value) {
+?>
+    <p><b><?php echo $key ?>: </b><?php echo $value ?></p>
+<?php } ?>
