95a3f13d41f9b1b9d012ce2a74f4304f87bef2e6
maakbarofficial <maakbarofficial@gmail.com>
2025-02-10 15:05:57 +0500

    Variables

--- index.php
+++ index.php
@@ -1,39 +1,30 @@
 <?php
 
-echo "Hello PHP.....!"
+$name = "Ali";
+$marks = 200.23;
+$age = 23;
+$isMarried = true;
+
+echo "My name is " . $name;
+echo "<br>My name is " . $marks;
+echo "<br>My name is $age";
+echo "<br>My name is " . $isMarried;
+echo "<br> <br>";
+
+var_dump($name);
+echo "<br>";
+var_dump($marks);
+echo "<br>";
+var_dump($age);
+echo "<br>";
+var_dump($isMarried);
+echo "<br>";
+
+# Rules to declare a variable
+# 1. Variable name should start with a letter or an underscore.
+# 2. Variable name should not start with a number.
+# 3. Variable name can only contain alpha-numeric characters and underscores.
+# 4. Variable names are case-sensitive.
 
 
-
-    // This is single comment
-
-    /*
-
-    This is multi line comment
-
-    */
-
-
-    # This is also single line comment
-
-    ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
-</head>
-
-<body>
-
-    <h1><?php echo "This is H1" ?></h1>
-
-
-
-
-</body>
-
-</html>
+?>
