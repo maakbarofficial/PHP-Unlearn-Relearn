d7755e8a14ca73919a895cc5fec1efc14c3c8208
maakbarofficial <maakbarofficial@gmail.com>
2025-02-11 16:28:08 +0500

    Working with radio buttons

--- index.php
+++ index.php
@@ -1,36 +1,3 @@
-<?php
-// PHP Global Variables - Superglobals
-// Some predefined variables in PHP are "superglobals", which means that they are always accessible, regardless of scope - and you can access them from any function, class or file without having to do anything special.
-// The PHP superglobal variables are:
-// $GLOBALS
-// $_SERVER
-// $_REQUEST
-// $_POST
-// $_GET
-// $_FILES
-// $_ENV
-// $_COOKIE
-// $_SESSION
-
-// echo "<pre>";
-// print_r($_GET);
-// exit;
-
-if (isset($_POST["submit"])) {
-    echo $_POST["username"] . "<br>";
-    echo $_POST["password"] . "<br>";
-
-    if (empty($_POST["username"])) {
-        echo "Username is empty";
-    } else {
-        echo "Username is not empty";
-    }
-}
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -43,26 +10,45 @@ if (isset($_POST["submit"])) {
 <body>
     <form action="index.php" method="post">
         <div>
-            <label>Username:</label>
-            <input type="text" name="username">
+            <label>Select any course:</label><br>
+            <input type="radio" name="course" value="PE">PE<br>
+            <input type="radio" name="course" value="PM">PM<br>
+            <input type="radio" name="course" value="CS">CS<br>
         </div>
         <br>
         <div>
-            <label>Password:</label>
-            <input type="password" name="password">
-        </div>
-        <br>
-        <div>
-            <button type="submit" name="submit">Log In</button>
+            <button type="submit" name="submit" value="Confirm">Submit</button>
         </div>
     </form>
 
-
-    <br>
-    <br>
     <br>
 
     <a href="./index.php">Home</a>
+
+    <br>
+    <br>
+
+    <?php
+    if (isset($_POST['course'])) {
+        echo "<pre>";
+        // print_r($_POST);
+        // exit;
+        $course = null;
+        if (isset($_POST["course"])) {
+            $course = $_POST["course"];
+        }
+
+        if ($course == "PE") {
+            echo "You have selected Pre Engineering Course";
+        } else if ($course == "CS") {
+            echo "You have selected Computer Science Course";
+        } else {
+            echo "You have selected Pre Medical Course";
+        }
+    }
+
+
+    ?>
 </body>
 
 </html>
