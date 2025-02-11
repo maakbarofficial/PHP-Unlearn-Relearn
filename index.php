e432abe582b60ab47e883cda7f2261a51736abf1
maakbarofficial <maakbarofficial@gmail.com>
2025-02-11 15:10:59 +0500

    Functions

--- index.php
+++ index.php
@@ -1,39 +1,21 @@
 <?php
 
-// Multidimensional Arrays
-// A multidimensional array is an array containing one or more arrays.
-// PHP supports multidimensional arrays that are two, three, four, five, or more levels deep
+// Function
+function myFun()
+{
+    echo "Welcome from my fun <br>";
+}
 
-$students = array(
-    array(
-        "Name" => "Ali Akbar",
-        "Role" => "Software Enginer",
-    ),
-    array(
-        "Name" => "Babar",
-        "Role" => "Senior Software Enginer",
-    ),
-    array(
-        "Name" => "Yasir",
-        "Role" => "Junior Software Enginer",
-    )
-);
-// print_r($students);
-echo "<br><br>";
-// print_r(array_keys($employees));
-// echo "<br><br>";
-// print_r(array_flip($employees));
-// echo "<br><br>";
-// print_r(array_reverse($employees));
-// echo "<br><br>";
+myFun();
 
-foreach ($students as $key => $value) {
-    echo "<prev>";
-    // print_r($value);
-    echo "<br><br>";
 
-    foreach ($value as $k => $v) {
-        echo "<br><br>";
-        echo $k . ": " . $v . "<br>";
+function sum($a, $op, $b)
+{
+    if ($op == "+") {
+        return "<h1>" . $a + $b . "</h1><br>";
+    } else if ($op == "-") {
+        return "<h1>" . $a - $b . "</h1><br>";
     }
 }
+
+echo (sum(2, "-", 5));
