783652edb734566ae0983a4f59fe660ba489fd89
maakbarofficial <maakbarofficial@gmail.com>
2025-02-11 16:46:56 +0500

    Working with checkboxes

--- index.php
+++ index.php
@@ -11,13 +11,16 @@
     <form action="index.php" method="post">
         <div>
             <label>Select any course:</label><br>
-            <input type="radio" name="course" value="PE">PE<br>
-            <input type="radio" name="course" value="PM">PM<br>
-            <input type="radio" name="course" value="CS">CS<br>
+            <!-- <input type="checkbox" name="Pre Engineering" value="PE">PE<br>
+            <input type="checkbox" name="Pre Medical" value="PM">PM<br>
+            <input type="checkbox" name="Computer Science" value="CS">CS<br> -->
+            <input type="checkbox" name="course[]" value="Pre Engineering">PE<br>
+            <input type="checkbox" name="course[]" value="Pre Medical">PM<br>
+            <input type="checkbox" name="course[]" value="Computer Science">CS<br>
         </div>
         <br>
         <div>
-            <button type="submit" name="submit" value="Confirm">Submit</button>
+            <button type="submit" name="confirm" value="Confirm">Submit</button>
         </div>
     </form>
 
@@ -29,23 +32,34 @@
     <br>
 
     <?php
-    if (isset($_POST['course'])) {
-        echo "<pre>";
+    // if (isset($_POST['course'])) {
+    //     echo "<pre>";
+    //     // print_r($_POST);
+    //     // exit;
+    //     $course = null;
+    //     if (isset($_POST["course"])) {
+    //         $course = $_POST["course"];
+    //     }
+
+    //     if ($course == "PE") {
+    //         echo "You have selected Pre Engineering Course";
+    //     } else if ($course == "CS") {
+    //         echo "You have selected Computer Science Course";
+    //     } else {
+    //         echo "You have selected Pre Medical Course";
+    //     }
+    // }
+
+    if (isset($_POST['confirm'])) {
+        // echo "<pre>";
         // print_r($_POST);
         // exit;
-        $course = null;
-        if (isset($_POST["course"])) {
-            $course = $_POST["course"];
-        }
-
-        if ($course == "PE") {
-            echo "You have selected Pre Engineering Course";
-        } else if ($course == "CS") {
-            echo "You have selected Computer Science Course";
-        } else {
-            echo "You have selected Pre Medical Course";
+        $courses = $_POST['course'];
+        foreach ($courses as $course) {
+            echo "You have Selected " . $course . "<br>";
         }
     }
+
 
 
     ?>
