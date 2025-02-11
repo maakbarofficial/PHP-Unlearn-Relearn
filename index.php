0d0eae9d5c2e9a23d057b2e2f70e1afe88af4e4b
maakbarofficial <maakbarofficial@gmail.com>
2025-02-11 15:46:39 +0500

    Superglobals

--- index.php
+++ index.php
@@ -1,33 +1,68 @@
 <?php
+// PHP Global Variables - Superglobals
+// Some predefined variables in PHP are "superglobals", which means that they are always accessible, regardless of scope - and you can access them from any function, class or file without having to do anything special.
+// The PHP superglobal variables are:
+// $GLOBALS
+// $_SERVER
+// $_REQUEST
+// $_POST
+// $_GET
+// $_FILES
+// $_ENV
+// $_COOKIE
+// $_SESSION
 
-// The empty() function checks whether a variable is empty or not.
-// This function returns false if the variable exists and is not empty, otherwise it returns true.
-// The following values evaluates to empty:
-// 0
-// 0.0
-// "0"
-// ""
-// NULL
-// FALSE
-// array()
+// echo "<pre>";
+// print_r($_GET);
+// exit;
 
-// The isset() function checks whether a variable is set, which means that it has to be declared and is not NULL.
-// This function returns true if the variable exists and is not NULL, otherwise it returns false.
-// Note: If multiple variables are supplied, then this function will return true only if all of the variables are set.
-// Tip: A variable can be unset with the unset() function.
+if (isset($_POST["submit"])) {
+    echo $_POST["username"] . "<br>";
+    echo $_POST["password"] . "<br>";
 
-$name = null;
-
-if (isset($name)) {
-    echo "Name is SET";
-} else {
-    echo "Name is not SET";
+    if (empty($_POST["username"])) {
+        echo "Username is empty";
+    } else {
+        echo "Username is not empty";
+    }
 }
 
-echo "<br><br>";
 
-if (empty($name)) {
-    echo "Name is SET";
-} else {
-    echo "Name is not SET";
-}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Superglobals</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <div>
+            <label>Username:</label>
+            <input type="text" name="username">
+        </div>
+        <br>
+        <div>
+            <label>Password:</label>
+            <input type="password" name="password">
+        </div>
+        <br>
+        <div>
+            <button type="submit" name="submit">Log In</button>
+        </div>
+    </form>
+
+
+    <br>
+    <br>
+    <br>
+
+    <a href="./index.php">Home</a>
+</body>
+
+</html>
