ee73124cbe8610258baa162fab4357c7952627ea
maakbarofficial <maakbarofficial@gmail.com>
2025-02-11 18:58:29 +0500

    Database Connection

--- index.php
+++ index.php
@@ -1,68 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Superglobals</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <div>
-            <label>Select any course:</label><br>
-            <!-- <input type="checkbox" name="Pre Engineering" value="PE">PE<br>
-            <input type="checkbox" name="Pre Medical" value="PM">PM<br>
-            <input type="checkbox" name="Computer Science" value="CS">CS<br> -->
-            <input type="checkbox" name="course[]" value="Pre Engineering">PE<br>
-            <input type="checkbox" name="course[]" value="Pre Medical">PM<br>
-            <input type="checkbox" name="course[]" value="Computer Science">CS<br>
-        </div>
-        <br>
-        <div>
-            <button type="submit" name="confirm" value="Confirm">Submit</button>
-        </div>
-    </form>
-
-    <br>
-
-    <a href="./index.php">Home</a>
-
-    <br>
-    <br>
-
-    <?php
-    // if (isset($_POST['course'])) {
-    //     echo "<pre>";
-    //     // print_r($_POST);
-    //     // exit;
-    //     $course = null;
-    //     if (isset($_POST["course"])) {
-    //         $course = $_POST["course"];
-    //     }
-
-    //     if ($course == "PE") {
-    //         echo "You have selected Pre Engineering Course";
-    //     } else if ($course == "CS") {
-    //         echo "You have selected Computer Science Course";
-    //     } else {
-    //         echo "You have selected Pre Medical Course";
-    //     }
-    // }
-
-    if (isset($_POST['confirm'])) {
-        // echo "<pre>";
-        // print_r($_POST);
-        // exit;
-        $courses = $_POST['course'];
-        foreach ($courses as $course) {
-            echo "You have Selected " . $course . "<br>";
-        }
-    }
+// Database Connection
+// MySQLi - works with MySQL
+// PDO (PHP Data Object) - works with 12 different databases
 
 
+// OOP Way
+$server = "localhost";
+$username = "root";
+$password = "root";
+$db = "testdbb";
 
-    ?>
-</body>
+// //Create Connection
+// $conn = new mysqli($server, $username, $password, $db);
 
-</html>
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+//     exit;
+// }
+
+// echo "Connection Successfully";
+
+// Procedural Way
+
+
+//Create Connection
+$conn = @mysqli_connect($server, $username, $password, $db);
+
+// echo "<pre>";
+// print_r($conn);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "<h1>Connection Successfully</h1>";
+}
